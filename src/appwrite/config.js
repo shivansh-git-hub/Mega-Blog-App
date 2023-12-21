2331be7243dc33805b72c2096df3537cd48cae91
Shivansh <shiv10ansh@gmail.com>
2023-12-21 09:07:55 +0530

    Added getPost

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -66,7 +66,17 @@ export class Service{
         }
     }
 
-    
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error", error);
+        }
+    }
 
 
 }
